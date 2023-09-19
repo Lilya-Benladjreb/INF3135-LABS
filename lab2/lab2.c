a1375a53a44a3c9e3cd8e334a146463e040c26ed
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2023-09-19 14:18:44 -0400

    ajout exo 1

---
+++ lab2/lab2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define NUM_SEC_DANS_1_JOUR 86400
+#define NUM_SEC_DANS_1_HEURE 3600
+
+
+void afficher_duree(unsigned int num_secondes){
+    int hr = 0;
+    int min = 0;
+    int sec = 0;
+
+    if(num_secondes > NUM_SEC_DANS_1_JOUR){
+        printf("Plus de 24h");
+    }else {
+        hr = num_secondes / NUM_SEC_DANS_1_HEURE;
+        num_secondes = num_secondes % 3600;
+        min = num_secondes / 60;
+        num_secondes = num_secondes % 60;
+        sec = num_secondes;
+
+        printf("%.2d:%.2d:%.2d\n", hr, min, sec);
+    }
+
+}
+
+int main(int argc, char * argv[]){
+    afficher_duree(8990);
+    return 0;
+}
