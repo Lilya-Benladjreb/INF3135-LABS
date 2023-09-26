42142276d8f7b740394493fc853c00b223851c13
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2023-09-26 14:09:19 -0400

    ajout quand delta == 0

--- lab3/quadradique.c
+++ lab3/quadradique.c
@@ -25,14 +25,20 @@ struct solution {
 struct solution solve_equation(struct equation eq){
     double b2 = pow(eq.b, 2);
     double delta = b2 - 4.0 * eq.a * eq.c;
-    struct solution sol;
 
+    struct solution sol;
+    sol.equation = eq;
+
+    if(delta == 0){
+        sol.num_solutions = 1;
+        sol.solutions.one_solution = -(eq.b)/2 * eq.a;
+    }
     return sol;
 }
 
 int main(int argc, char* argv[]){
     double a = 1;
-    double b = 1;
+    double b = -2;
     double c = 1;
 
     struct equation eq = {.a=a, .b=b, .c=c};
@@ -44,7 +50,7 @@ int main(int argc, char* argv[]){
 
     struct solution sol = solve_equation(eq);
 
-    printf("solution: %d\n", sol.num_solutions);
+    printf("solution: %d, %.2f\n", sol.num_solutions, sol.solutions.one_solution);
 
     return 0;
 }
