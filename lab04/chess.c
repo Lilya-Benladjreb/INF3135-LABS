e8f795ebbbca2415560313179463eb1cf7f0782b
Lilya Benladjreb <lilyacbenladjreb@gmail.com>
2023-10-03 14:26:58 -0400

    ajout methode initialisation echiquier

--- lab04/chess.c
+++ lab04/chess.c
@@ -56,9 +56,40 @@ void ajouter_piece(struct echiquier *echiquier,
  * @param echiquier  L'échiquier à initialiser
  */
 void initialiser_echiquier(struct echiquier *echiquier) {
-    for(i < 32){
-        ajouter_piece();
+    for(int r = 0; r < 8; r++){
+        for(int c = 0; c < 8; c++){
+            echiquier->cases[r][c] = NULL;
+        }
     }
+
+    for(int e = 0; e < 8; e++){
+        unsigned int identifiant = e;
+        ajouter_piece(echiquier, identifiant, 6, e,BLANC, PION);
+        ajouter_piece(echiquier, identifiant+16, 1, e,NOIR, PION);
+    }
+
+    //Ajouter le reste des pièces à la main
+    ajouter_piece(echiquier, 8,0,0, BLANC, TOUR);
+    ajouter_piece(echiquier, 9,0,7, BLANC, TOUR);
+    ajouter_piece(echiquier, 24,0,7, NOIR, TOUR);
+    ajouter_piece(echiquier, 25,7,0, NOIR, TOUR);
+
+    ajouter_piece(echiquier, 10,0,1, BLANC, CAVALIER);
+    ajouter_piece(echiquier, 11,0,1, BLANC, CAVALIER);
+    ajouter_piece(echiquier, 26,7,1, NOIR, CAVALIER);
+    ajouter_piece(echiquier, 27,7,1, NOIR, CAVALIER);
+
+    ajouter_piece(echiquier, 12,0,2, BLANC, FOU);
+    ajouter_piece(echiquier, 13,0,2, BLANC, FOU);
+    ajouter_piece(echiquier, 28,7,2, NOIR, FOU);
+    ajouter_piece(echiquier, 29,7,2, NOIR, FOU);
+
+    ajouter_piece(echiquier, 14,0,3, BLANC, DAME);
+    ajouter_piece(echiquier, 30,7,3, NOIR, DAME);
+
+    ajouter_piece(echiquier, 15,0,4, BLANC, ROI);
+    ajouter_piece(echiquier, 31,7,4, NOIR, ROI);
+
 }
 
 /**
